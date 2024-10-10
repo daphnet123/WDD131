d743638905c228f1293bd902f7ec446f98a37837
daphnet123 <try22002@byui.edu>
2024-10-10 17:48:08 -0500

    fixing file path folder mistake

---
+++ ponder/W04-debugging-activity.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+
+function circleArea(radius) {
+  return radius * radius * PI; 
+}
+
+let area1 = circleArea(3);
+console.log("Area1:", area1); 
+
+let area2 = circleArea(4);
+console.log("Area2:", area2); 
