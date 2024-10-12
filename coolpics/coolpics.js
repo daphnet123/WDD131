18741463090b9a5b9126c415365e9e17d4d4afd5
daphnet123 <try22002@byui.edu>
2024-10-12 13:21:29 -0500

    Cool Pics 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,13 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuButton = document.querySelector('.menu-button');
     const navLinks = document.querySelector('.nav-links');
+    const viewer = document.querySelector('.viewer');
+    const viewerImage = viewer.querySelector('img');
+    const closeViewerButton = document.querySelector('.closse-viewer');
   
     menuButton.addEventListener('click', function () {
-      if (navLinks.style.display === 'block') {
-        navLinks.style.display = 'none';
+      navLinks.classList.toggle('hide');
+    });
+
+    function handleResize() {
+      if (window.innerWidth > 1000) {
+        navLinks.classList.remove('hide');
       } else {
-        navLinks.style.display = 'block';
+        navLinks.classList.add('hide');
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    document.querySelector('gallery').addEventListener('click', function (event) {
+      if (event.target.tagName === 'IMG') {
+        const src = event.target.src.replace('-sm.jpeg', '-full.jpeg');
+        viewerImage.src = src;
+        viewer.classList.remove('hide');
       }
     });
+
+    closeViewerButton.addEventListener('click', function () {
+      viewer.classList.add('hide');
   });
+});
   
